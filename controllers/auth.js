bd89de5ea0b05af1e7f706f370256588726567de
pritammimo <crpritamsaha@gmail.com>
2023-08-25 11:08:21 +0530

    registration api done

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,49 +2,22 @@
 import { connectToDb } from "../src/db/db.js";
 import { AuthSchema } from "../src/db/schema/auth-schema.js";
  import bcrypt from "bcryptjs";
+import { asyncHandler } from "../middleware/async.js";
+import { ErrorResponse } from "../utils/errorResponse.js";
 // import jwt from "jsonwebtoken";
-
-export const register = async(req, res) => {
-   const {email,password,username,role}=req.body;
-   if(email ===undefined ||password===undefined || username===undefined || role ===undefined){
-    return res.status(409).json({message:"Please add all the details"})
-   }
+export const register=asyncHandler(async(req,res,next)=>{
+  const {email,password,username,role}=req.body;
    const db=await connectToDb();
-   const find = await db.select().from(AuthSchema).where(eq(AuthSchema.email,req.body.email));
-   if(find?.length>0){
-    return res.status(409).json({message:"user already exists"})
-   }
        const salt = bcrypt.genSaltSync(10);
    const hash = bcrypt.hashSync(req.body.password, salt);
     await db.insert(AuthSchema).values({
-      email:req.body.email,
-     username:req.body.username,
+      email,
+      username,
       password:hash,
-      role:req.body.role
+      role
     });
-    
-  // console.log("up",find);
     return res.status(200).json({message:"user has been created"});
-  //CHECK EXISTING USER
-//   const q = "SELECT * FROM users WHERE email = ? OR username = ?";
-
-//   db.query(q, [req.body.email, req.body.username], (err, data) => {
-//     if (err) return res.status(500).json(err);
-//     if (data.length) return res.status(409).json("User already exists!");
-
-//     //Hash the password and create a user
-//     const salt = bcrypt.genSaltSync(10);
-//     const hash = bcrypt.hashSync(req.body.password, salt);
-
-//     const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
-//     const values = [req.body.username, req.body.email, hash];
-
-//     db.query(q, [values], (err, data) => {
-//       if (err) return res.status(500).json(err);
-//       return res.status(200).json("User has been created.");
-//     });
-//   });
-};
+})
 
 export const login = (req, res) => {
   //CHECK USER
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "../routes/auth.js";
 // import userRoutes from "../routes/users.js";
 // import postRoutes from "../routes/posts.js";
 import cookieParser from "cookie-parser";
+import { errorHandler,notFound } from "../middleware/error.js";
 // import multer from "multer";
 
 const app = express();
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use(errorHandler);
+app.use(notFound)
 app.listen(8800, () => {
     console.log("Connected!");
   });
